5c48cb0d8c776b17c53f33c164731166115e9bfa
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-02-20 00:05:37 -0300

    configurar firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  api: `https://damp-tor-97294.herokuapp.com`
+  api: `https://damp-tor-97294.herokuapp.com`,
+  firebaseConfig : {
+    apiKey: "AIzaSyDtrieoIgUGaSs6mQ-HjeysHBBWmaTIX4I",
+    authDomain: "loja-virtual-6bf89.firebaseapp.com",
+    databaseURL: "https://loja-virtual-6bf89-default-rtdb.firebaseio.com",
+    projectId: "loja-virtual-6bf89",
+    storageBucket: "loja-virtual-6bf89.appspot.com",
+    messagingSenderId: "1045029356516",
+    appId: "1:1045029356516:web:848b1ceff3b15e2068fdf1"
+  }
 };
 
 /*
